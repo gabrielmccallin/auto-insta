49453a93b9064e2c96f8ee3db492afcef7212390
Gabriel McCallin <gabriel.mccallin@hey.com>
2022-01-02 23:41:40 +0000

    🔍 OMG, actually pass error

--- src/routes/post-photo/post-photo.ts
+++ src/routes/post-photo/post-photo.ts
@@ -20,7 +20,7 @@ export const photo = async (context: ContextWithUpload) => {
     formData = parsed.fields;
     files = parsed.files || [];
   } catch (_error) {
-    return respond(responseCodes.invalidFormData, context);
+    return respond(responseCodes.invalidFormData, context, _error);
   }
 
   if (formData && !validatePayload(formData)) {
